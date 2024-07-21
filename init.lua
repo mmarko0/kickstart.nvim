a9be0a9db0d46ca7297ff07546aa7215cbd4444c
mmarko <marik.marko.96@gmail.com>
2024-07-21 15:34:15 +0300

    Set default cursor

--- init.lua
+++ init.lua
@@ -75,6 +75,8 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Default cursor
+vim.opt.guicursor = ''
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
